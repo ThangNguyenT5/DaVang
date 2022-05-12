5c6372601a7596e7bf55afca52add87adf5247f2
ThangNguyenT5 <toanthangttp@gmail.com>
2022-05-13 01:43:21 +0700

    create

---
+++ app.js
@@ -0,0 +1,16 @@
+let index_slide=0;
+slide_show()
+
+
+function slide_show() {
+    let i;
+    let slide = document.getElementsByClassName("slider_item");
+    for (i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    }
+    index_slide++;
+    if (index_slide > slide.length) {index_slide = 1}
+    slide[index_slide-1].style.display ="block";
+
+    setTimeout(slide_show, 2000);
+};
